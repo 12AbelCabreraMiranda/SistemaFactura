f53446b6c3c746a6cffd911804f4618f41cf418a
12AbelCabreraMiranda <leg.abelr@gmail.com>
2021-04-15 21:16:49 -0600

    Mojoras de interfaz en las tablas

---
+++ Sistema_Factura/Controllers/ClienteController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sistema_Factura.DataContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sistema_Factura.Controllers
+{
+    public class ClienteController : Controller
+    {
+        private readonly Sistema_FacturaContext _context;
+        public ClienteController(Sistema_FacturaContext context) => _context = context;
+
+
+        public async Task<IActionResult> Cliente()
+        {
+            return View(await _context.Cliente.ToListAsync());
+        }
+    }
+}
