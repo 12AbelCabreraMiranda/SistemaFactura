647ee0d0d483785e8867c534915943e791536e33
12AbelCabreraMiranda <leg.abelr@gmail.com>
2021-04-20 17:50:35 -0600

    backend y frontend en agregar productos temporales desarrollado

--- Sistema_Factura/Controllers/TemporalProductoController.cs
+++ Sistema_Factura/Controllers/TemporalProductoController.cs
@@ -252,5 +252,110 @@ namespace Sistema_Factura.Controllers
 
             return RedirectToAction(nameof(BuscarProducto));
         }
+
+
+        //TESTING AGREGAR PRODUCTO TEMPORAL SIN NIT
+        [HttpPost]        
+        public IActionResult AgregarProductoTemporal(AgregarProducto agregarProductoModel)
+        {
+            //Procedimiento para los subtotales de los productos
+            var _subTotal = agregarProductoModel.PrecioProductoCompra * agregarProductoModel.Cantidad;
+
+            if (agregarProductoModel.Cantidad <= 0)
+            {
+                TempData["messageCantidad"] = "La cantidad debe ser mayor a cero";
+                return RedirectToAction(nameof(BuscarProducto));
+            }
+            else
+            {
+                if (agregarProductoModel.PrecioProductoCompra == 0)
+                {
+                    TempData["messageSinRegistros"] = "Debe agregar registro de un producto";
+
+                    return RedirectToAction(nameof(BuscarProducto));
+                }
+                else
+                {
+                    var add_tempProducto = new TempProducto()
+                    {
+                        Cantidad_temp = agregarProductoModel.Cantidad,
+                        PrecioVenta_temp = agregarProductoModel.PrecioProductoCompra,
+                        ProductoId = agregarProductoModel.ProductoId,
+                        SubTotal_temp = _subTotal,
+                        IdCliente_temp = 1
+                    };
+                    _context.TempProducto.Add(add_tempProducto);
+                    _context.SaveChanges();
+
+                }
+            }
+            return RedirectToAction(nameof(BuscarProducto));
+        }
+
+        
+        //GUARDAR FACTURA VERSION 2
+        [HttpPost]
+        public IActionResult GuardarFactura2(AgregarProducto agregarProducto)
+        {
+            //Consulta de los registros en el Modelo: TempProducto
+            var tempPro = (from p in _context.TempProducto
+                           select p).ToList();
+            
+            //CONSULTA NIT Y EXTRA EL ID DEL CLIENTE
+            var _idNit = _context.Cliente.Where(c=>c.Nit.Equals(agregarProducto.NitCliente_temp)).FirstOrDefault();
+
+            //Obtener suma Total de los productos a vender de la factura
+            var _totalFactura = (from s in _context.TempProducto
+                                 select s.SubTotal_temp).Sum();
+
+            //Fecha del sistema
+            DateTime FechaSistema = System.DateTime.Now;
+
+            if (!tempPro.Count.Equals(0))
+            {
+                //Guardar en Modelo Factura
+                var _factura = new Factura
+                {
+                    TotalPrecio = _totalFactura,
+                    ClienteId = _idNit.ClienteId,
+                    FechaFactura = FechaSistema,
+                    EstadoFactura = 1
+                };
+                _context.Factura.Add(_factura);
+                _context.SaveChanges();
+
+                //Guardar Rango de registros en Modelo: DetalleFactura
+                if (!tempPro.Count.Equals(0))
+                {
+                    foreach (var item in tempPro)
+                    {
+                        //Guarda en Modelo Detalle Factura
+                        var _detalleFactura = new DetalleFactura
+                        {
+                            Cantidad = item.Cantidad_temp,
+                            PrecioVenta = item.PrecioVenta_temp,
+                            FacturaId = _factura.FacturaId,// El ID, Lo he tomado como el numero de factura
+                            ProductoId = item.ProductoId,
+                            SubTotal = item.SubTotal_temp
+                        };
+                        _context.DetalleFactura.AddRange(_detalleFactura);
+                        _context.SaveChanges();
+                    }
+                }
+                //Elimina los registros del modelo: TempProducto            
+                var x = (from y in _context.TempProducto
+                         select y).ToList();
+                _context.TempProducto.RemoveRange(x);
+                _context.SaveChanges();
+
+                return RedirectToAction(nameof(BuscarProducto));
+            }
+            else
+            {
+                TempData["messageNoFactura"] = "No se puede crear Factura sin registros";
+
+                return RedirectToAction(nameof(BuscarProducto));
+            }
+        }
     }
 }
