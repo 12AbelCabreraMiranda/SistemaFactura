328cb52c3e00fa3b0e3f7b8a1cc9f6a50cb3bbcd
12AbelCabreraMiranda <leg.abelr@gmail.com>
2021-05-22 14:10:22 -0600

    Datatable disenio personalizado

--- Sistema_Factura/Areas/CrearFactura/Controllers/CrearFacturaController.cs
+++ Sistema_Factura/Areas/CrearFactura/Controllers/CrearFacturaController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sistema_Factura.DataContext;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,10 +10,34 @@ namespace Sistema_Factura.Areas.CrearFactura.Controllers
 {
     public class CrearFacturaController : Controller
     {
+        private readonly Sistema_FacturaContext _context;
+        public CrearFacturaController(Sistema_FacturaContext context) => _context = context;
+
+
         [Area("CrearFactura")]
         public IActionResult CrearFactura()
         {
             return View();
+        }
+
+
+        //******CONTROLADOR FUNCIONA DATATABLE CON AJAX************+
+        [HttpGet]
+        public async Task<IActionResult> DataTable()
+        {
+            var produc = await _context.Producto.Where(x => x.EstadoProducto == 1).ToListAsync();
+            //var productoAjax = (from p in _context.Producto
+
+            //                    select new
+            //                    {         
+            //                        p.ProductoId,
+            //                        p.CodigoProducto,
+            //                        p.NombreProducto, 
+            //                        p.PrecioProductoCompra
+            //                    }).ToListAsync();
+
+
+            return Json(new { data = produc });
         }
     }
 }
--- Sistema_Factura/wwwroot/js/DataTable.js
+++ Sistema_Factura/wwwroot/js/DataTable.js
@@ -7,3 +7,50 @@
     }).fail(manejarErrorAjax);
 
 });
+
+
+
+
+var dataTable;
+$(document).ready(function () {    
+    dataTable = $('#DT_Load').DataTable({
+        "ajax": {
+            "url": "/CrearFactura/DataTable",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            {
+                "data": "codigoProducto",
+                "width": "10%"
+            },
+            {
+                "data": "nombreProducto",
+                "width": "50%"
+            },
+            {
+                "data": "precioProductoCompra",
+                "width": "10%"
+            },
+            {
+                "data": "productoId",
+                "width": "10%",
+                "render": function (data) {
+                    return `<div class='text-center'>                    
+                                    <a onclick=Delete('/Product/DeleteByDataApiJson?TempProductoId='+${data}) class='btn btn-danger text-white' style='cursor:pointer'>Delete</a>                       
+                                 </div>`
+
+                }
+            }
+
+        ],
+        "language": {
+            "url": "//cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json"//LENGUAJE ESPAÑOL DATATABLE
+        },
+        "width": "100%",
+        "bLengthChange": false,//Desabilita combobox cantidad de registro a ver en la tabla
+        "ordering": false,// Ordering (Sorting on Each Column)will Be Disabled
+        "bAutoWidth": false,//Desactiva el ancho automatico de cada columna
+        "lengthMenu": [[5,10, 25, 50, -1], [5,10, 25, 50, "All"]]//INICIALIZA 5 REGISTRO
+    });
+});
