412f76f63e97dd7f31345d9a4ac9e07e4ed36997
12AbelCabreraMiranda <leg.abelr@gmail.com>
2021-05-30 10:31:20 -0600

    diseniando vista crear factura

--- Sistema_Factura/wwwroot/js/DataTable.js
+++ Sistema_Factura/wwwroot/js/DataTable.js
@@ -1,4 +1,5 @@
-﻿$(function () {
+﻿//FUNCION MUESTRA DATOS DE LA TABLA TEMPORAL
+$(function () {
     
     var url = "/TemporalProducto/ProductosAgregados";
 
@@ -10,7 +11,7 @@
 
 
 
-
+//FUNCION DATATABLE PRODUCTOS
 var dataTable;
 $(document).ready(function () {    
     dataTable = $('#DT_Load').DataTable({
@@ -47,7 +48,7 @@ $(document).ready(function () {
                 "width": "10%",
                 "render": function (data) {
                     return `<div class='text-center'>                                    
-                                <a onclick=Agregar() class='btn btn-info text-white' style='cursor:pointer'> <i class='bi bi-cart-plus'></i></a>                       
+                                <a onclick=Agregar('/Product/DeleteByDataApiJson?TempProductoId='+${data})  class='btn btn-info text-white' id='idProducto' style='cursor:pointer'> <i class='bi bi-check2-square'></i></a>                       
                             </div>`
 
                 }
@@ -59,14 +60,15 @@ $(document).ready(function () {
             "url": "//cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json"//LENGUAJE ESPAÑOL DATATABLE
         },
         "width": "100%",
-        "bLengthChange": false,//Desabilita combobox cantidad de registro a ver en la tabla
+        "bLengthChange": false,//Disable select record quantity to view in table
         "ordering": false,// Ordering (Sorting on Each Column)will Be Disabled
-        "bAutoWidth": false,//Desactiva el ancho automatico de cada columna
-        "lengthMenu": [[5, 10, 25, 50, -1], [5, 10, 25, 50, "All"]],//INICIALIZA 5 REGISTRO
+        "bAutoWidth": false,//Disable the automatic width of each column
+        "lengthMenu": [[5, 10, 25, 50, -1], [5, 10, 25, 50, "All"]],//Initialize view of 5 records in table
        
     });
 });
 
+//FUNCION AGREGAR PRODUCTOS A TABLA TEMPORAL
 function Agregar() {
     
     alert($("#txtCantidad").val());
